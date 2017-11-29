e9714cf8519e7479ef70e22a5d104bffb5d3fcc2
Nurul Arifin <nurularifin120399@gmail.com>
2017-11-29 20:45:52 +0700

    Pertemuan1Mobile

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>My Application</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/blog.css" rel="stylesheet">
+  </head>
+  <body>
+  	@include('layouts.nav')
+
+  	<div class="blog-header">
+      <div class="container">
+        <h1 class="blog-title">My Application</h1>
+        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+      </div>
+    </div>
+
+    <div class="container">
+
+    <div class="row">
+    	<div class="col-sm-8 blog-main">
+  			@yield('content')
+		</div><!-- /.blog-main -->  		  			
+  		@include('layouts.sidebar')
+  	</div><!-- /.row -->
+    </div><!-- /.container -->
+    @include('layouts.foo')
+</body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('content')
+          <div class="blog-post">
+          <nav class="blog-pagination">
+            <a class="btn btn-outline-primary" href="#">Older</a>
+            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+          </nav>        
+@endsection
